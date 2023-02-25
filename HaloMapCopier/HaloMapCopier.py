11c1d0fa709d90cc12de48ba22a4f20df0bd69c4
李兴鹏 <96003348+ImMrLee@users.noreply.github.com>
2023-02-25 14:03:47 +0800

    HaloMapCopier Now Uploaded
    GOGOGO

---
+++ HaloMapCopier/HaloMapCopier.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+print("This Program Can Help You To Install Your Map. Thanks By Miste Lee")
+file=input(r"What's The Map Name? Type the full name print: ")
+target=input(r"Now Tell Me The Drive Letter That Installed The Game MCC: ")
+game=(":\\SteamLibrary\\steamapps\\common\\Halo The Master Chief Collection")
+gen=int(input("Now Tell Me The Gen of Halo "))
+if (gen == 1):
+    halo=("\\halo1\maps")
+elif (gen == 2):
+    halo=("\\halo2\maps")
+elif (gen == 3):
+    halo=("\\halo3\maps")
+elif (gen == 4):
+    halo=("\\halo4\maps")
+
+shutil.copy (file, target+game+halo)
+print ("Done :DDDD")
+os.system(exit())
